53ce3878db94f56722fb2a91493d1da9bfb87c3b
Jonathan MLYNARCZYK <jmlynarc@e2r9p20.42.fr>
2017-12-03 19:24:59 +0100

    Corrections diverses

--- Fillit_files/backtracking.c
+++ Fillit_files/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: jmlynarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 11:46:04 by jmlynarc          #+#    #+#             */
-/*   Updated: 2017/11/24 15:40:54 by jmlynarc         ###   ########.fr       */
+/*   Updated: 2017/12/03 17:56:03 by jmlynarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,45 @@ int				try_bloc_order(char ***array, char **grid, t_order *order)
 }
 
 t_solution	**append_solution(t_order *order, t_solution **solutions,
-	char **grid)
+		char **grid)
 {
 	t_solution	*new_node;
 
 	//if (solutions && *solutions && (*solutions)->square_size <= get_square_size(grid))
-		//return (solutions);
+	//return (solutions);
 	if (!(solutions) || !(new_node = (t_solution *)malloc(sizeof(t_solution))))
 		return (NULL);
 	new_node->order = order;
 	new_node->square_size = get_square_size(grid);
 	new_node->next = *solutions;
 	*solutions = new_node;
+	return (solutions);
+}
+
+t_solution		**optimize_solution(char ***grid, char ***array, t_order *order, t_solution **solutions)
+{
+	int		square_size;
+
+	square_size = 120;
+	// No solution found for the current order in the current grid
+	if (!(try_bloc_order(array, *grid, order)))
+		return (solutions);
+	// A solution has been found : should be optimized if possible to fit a narrower square
+	// Get current square size
+	square_size = get_square_size(*grid);
+	// Try to fit the blocs into a square with a size reduced by one
+	while ((try_bloc_order(array, *grid, order)))
+		*grid = get_grid(--square_size, grid);
+	// The last square size was too small for the blocs to fit :
+	// the grid is resized to the smallest size in which the blocs fit.
+	*grid = get_grid(++square_size, grid);
+	// the new solution is added to the list and returned
+	solutions = append_solution(orddup(order), solutions, *grid);
+	/*
+	 * TODO: while solution can be placed, reduce the size of the grid
+	 * by one and try again. When not working : keep the size+1 for
+	 * next resolution.
+	 */
 	return (solutions);
 }
 
@@ -60,12 +87,14 @@ t_solution	**append_solution(t_order *order, t_solution **solutions,
  * "order" is supposed already allocated at the good size, and cleaned.
  */
 t_solution		**place_bloc(int index, char ***grid, char ***array,
-	t_order *order)
+		t_order *order)
 {
+	int						square_size;
 	int						bloc_number;
 	static t_solution		**solutions = NULL;
 
 	bloc_number = -1;
+	square_size = 120;
 	if (!(solutions))
 	{
 		if (!(solutions = (t_solution **)malloc(sizeof(t_solution *))))
@@ -77,12 +106,24 @@ t_solution		**place_bloc(int index, char ***grid, char ***array,
 		if (!elt_in_array(bloc_number, order))
 		{
 			(order->order)[index] = bloc_number;
+			if (index + 1 == order->length)
+				solutions = optimize_solution(grid, array, order, solutions);
 			// Si dernier bloc mis en ordre : tente une résolution
-			if (index + 1 == order->length && (try_bloc_order(array, *grid, order)))
+			// A solution has been found in the smallest square so far.
+			// Could be optimized to be smaller.
+/*			if (index + 1 == order->length && (try_bloc_order(array, *grid, order)))
 			{
+				square_size = get_square_size(grid);
+				while ((try_bloc_order(array, *grid, order)))
+					*grid = get_grid(--square_size, grid);
 				solutions = append_solution(orddup(order), solutions, *grid);
 				*grid = get_grid((*solutions)->square_size , grid);
-			}
+				/ *
+				 * TODO: while solution can be placed, reduce the size of the grid
+				 * by one and try again. When not working : keep the size+1 for
+				 * next resolution.
+				 * /
+			}*/
 			else if (index + 1 < order->length)
 				place_bloc(index + 1, grid, array, order);
 		}
--- Fillit_files/tetriminos_check.c
+++ Fillit_files/tetriminos_check.c
@@ -6,25 +6,52 @@
 /*   By: jmlynarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 17:20:15 by jmlynarc          #+#    #+#             */
-/*   Updated: 2017/11/19 17:46:13 by jmlynarc         ###   ########.fr       */
+/*   Updated: 2017/12/03 19:24:33 by jmlynarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+/*
+ * FIXME: mauvaise methode de calcul : si des diagonales mais des consecutifs
+ * TODO: essayer de faire un path de 3 deplacements horizontaux ou verticaux
+ */
 int		square_is_single(char **tetri, int l, int c)
 {
 	if (tetri[l][c] == '.')
 		return (0);
-	if (l > 0 && tetri[l - 1][c] == '#')
+	if (l > 0 && tetri[l - 1][c] != '.')
 		return (0);
-	if (l < 3 && tetri[l + 1][c] == '#')
+	if (l < 3 && tetri[l + 1][c] != '.')
 		return (0);
-	if (c > 0 && tetri[l][c - 1] == '#')
+	if (c > 0 && tetri[l][c - 1] != '.')
 		return (0);
-	if (c < 3 && tetri[l][c + 1] == '#')
+	if (c < 3 && tetri[l][c + 1] != '.')
 		return (0);
 	return (1);
+}
+
+int		bloc_is_tetri(char **tetri)
+{
+	int		l;
+	int		c;
+	int		square;
+
+	l = -1;
+	square = 0;
+	while (++l < 4 && tetri[l][c] == '.')
+	{
+		c = 0;
+		while (c < 4 && tetri[l][c] == '.')
+			c++;
+	}
+	square = 1;
+	// First square is found
+	while (square <= 4 && l < 4)
+	{
+		
+	}
+	return (square == 4);
 }
 
 int		tetrimino_is_correct(char **tetri)
@@ -37,13 +64,12 @@ int		tetrimino_is_correct(char **tetri)
 	squares = 0;
 	while (l < 4)
 	{
-		c = 0;
-		while (c < 4)
+		c = -1;
+		while (++c < 4)
 		{
-			squares += (tetri[l][c] == '#');
+			squares += (tetri[l][c] != '.');
 			if (squares > 4 || square_is_single(tetri, l, c))
 				return (0);
-			c++;
 		}
 		l++;
 	}
